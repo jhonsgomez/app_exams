3bdc9739d99f0d2f7859c41063afbd46867b0f13
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2025-11-22 16:34:25 -0500

    first commit

--- project/settings.py
+++ project/settings.py
@@ -139,12 +139,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "/tmp/media/"
-
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets")
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
