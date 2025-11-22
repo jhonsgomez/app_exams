a9b687ca80f1652e55f2c9cf0f6efb3ab4225c43
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2025-11-22 16:03:52 -0500

    first commit

--- project/settings.py
+++ project/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "assets")
 
 # Default primary key field type
